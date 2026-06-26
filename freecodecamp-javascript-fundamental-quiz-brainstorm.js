082f76e3b4374de575f06d8da34334c9bef05b10
rubengitdev <rubencahyadi504@gmail.com>
2026-06-26 15:43:15 +0700

    JS fundamental quiz brainstorm

---
+++ freecodecamp-javascript-fundamental-quiz-brainstorm.js
@@ -0,0 +1,24 @@
+// let stringArray = [8, 9, 10].toString();
+// console.log(stringArray);
+
+// console.log(new Number("123"));
+
+// console.log(Number(true));
+// console.log(Number(false));
+
+// console.log(Number(undefined));
+// console.log(Number(null));
+
+// function trackTotal(initialValue) {
+//   let total = initialValue;
+//   return function (increment) {
+//     total += increment;
+//     return total;
+//   };
+// }
+
+// let track = trackTotal(1);
+// console.log(track(2));
+// console.log(track(3));
+
+console.log(new String("Hello World"));
