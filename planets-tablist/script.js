583cebaa58c37770fd12f5cd842db5a8cd0006d0
rubengitdev <rubencahyadi504@gmail.com>
2026-06-30 00:35:23 +0700

    Planets Facts

---
+++ planets-tablist/script.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll(`[role="tab"]`);
+const panels = document.querySelectorAll(`[role="tabpanel"]`);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tab.setAttribute("aria-selected", "false");
+
+    panels.forEach((panel) => {
+      panel.setAttribute("hidden", true);
+    });
+
+    tab.setAttribute("aria-selected", "true");
+
+    const associatedPanel = tab.getAttribute("aria-controls");
+    const panel = document.getElementById(associatedPanel);
+    panel.hidden = false;
+  });
+});
