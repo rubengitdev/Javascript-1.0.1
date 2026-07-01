1f883d9eda8b5d9f98e75d4224ef8b1e1579b573
rubengitdev <rubencahyadi504@gmail.com>
2026-07-02 02:42:58 +0700

    Spinal Case Converter

---
+++ spinal-case-converter/script.js
@@ -0,0 +1,8 @@
+function spinalCase(str) {
+  return str
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .replace(/[\s_]+/g, "-")
+    .toLowerCase();
+}
+
+console.log(spinalCase("AllThe-small Things"));
